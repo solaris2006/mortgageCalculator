1827b368990d06b3e8f676209cc89dde509a4885
solaris2006 <cristian.stefan@gmail.com>
2020-11-25 10:36:56 +0200

    Delete test files

--- src/com/company/Conditionals.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Conditionals {
-    public static void main(String[] args) {
-        int temperature = 12;
-        boolean isWarm = temperature > 20 || temperature < 30;
-        System.out.println(isWarm);
-    }
-}
