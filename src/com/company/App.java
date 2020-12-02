18e1affc632036823fbb10e0086c1180f55789e5
solaris2006 <cristian.stefan@gmail.com>
2020-12-02 08:49:54 +0200

    Changed to var

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -5,9 +5,9 @@ public class App {
     public static void run() {
 
         var read = new Read();
-        int principal = (int) read.readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) read.readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte) read.readNumber("Period (Years): ", 1, 30);
+        var principal = (int) read.readNumber("Principal: ", 1000, 1_000_000);
+        var annualInterest = (float) read.readNumber("Annual Interest Rate: ", 1, 30);
+        var years = (byte) read.readNumber("Period (Years): ", 1, 30);
         var mortgage = new Mortgage(principal,annualInterest, years);
         mortgage.printMortgage(mortgage.calculateMortgage());
         mortgage.printPaymentSchedule(years);
